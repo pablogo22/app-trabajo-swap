68c3caafd89e791ea91aa879f667220175eabefa
Pablo Gálvez Ortigosa <86744039+pablogo22@users.noreply.github.com>
2025-05-21 11:20:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Se muestra una cabecera con mi usuario de la UGR
-echo "<h1>SWAP - pablogalor</h1>";
+echo "<h1>SWAP - pablogalor y jlmera</h1>";
 
 // Se muestra la IP del servidor
 echo "La IP del servidor es: " . $_SERVER['SERVER_ADDR'];
